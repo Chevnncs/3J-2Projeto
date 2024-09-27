64f2303e34dc696025551bd8f7bf659ed031fba9
Arthur <110104912+Chevnncs@users.noreply.github.com>
2024-09-27 09:38:01 -0300

    27/09

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -2,8 +2,10 @@ const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 
 async function visualizarDadosGlobais(){
  const res = await fetch(url);
- const dados = await res.json();
- console.log(res);
+ const dados = await res.json()
+ console.log(dados)
+ const paragrafo = document.createElement('p');
+ paragrafo.classList.add('graficos-texto')
 }
 
 visualizarDadosGlobais();
