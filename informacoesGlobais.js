303402120c22d2f9a9e4d7e1778573c3a3d91a11
Arthur <110104912+Chevnncs@users.noreply.github.com>
2024-10-03 08:08:43 -0300

    03/10

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -2,11 +2,18 @@ const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 
 async function visualizarDadosGlobais(){
  const res = await fetch(url);
- const dados = await res.json()
+ const secao = document.querySelector('.graficos-conterner')
+ const dados = await res.json();
+ const totalPessoasMundo = dados.total_pessoas_mundo
+ const totalPessoasConectadas = dados.total_pessoas_conectadas
+ const tempoMedio = dados.tempo_medio
  console.log(dados)
+ console.log(dados.tempo_medio)
+ console.log(dados.total_de_pessoas)
  const paragrafo = document.createElement('p');
  paragrafo.classList.add('graficos-texto')
-
+ paragrafo.innerHTML = `Voce sabia que no mundo tem ${totalPessoasMundo} de habitantes e destes ${totalPessoasConectadas} estao conectadas a alguma rede social e ficam em média ${tempoMedio} conectadas.`
+ secao.appendChild(paragrafo);
 }
 
 visualizarDadosGlobais();
